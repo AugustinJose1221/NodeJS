24c18336d0a26148dd3296c1c1bbdc8acb3815b1
Augustin Jose <augustinjose1221@gmail.com>
2020-09-03 23:04:22 +0530

    Added node modules and files

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+const port = 8001;
+
+io.on("connection", socket => {
+  console.log("a user connected :D");
+  socket.on("chat message", msg => {
+    console.log(msg);
+    io.emit("chat message", msg);
+  });
+});
+server.listen(port, '192.168.43.168', () => console.log("server running on port:" + port));
