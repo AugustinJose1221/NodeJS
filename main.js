ca867055e584538f8f9d75d08c7cc4114609b775
Augustin Jose <augustinjose1221@gmail.com>
2020-09-04 11:14:24 +0530

    Minor Updates

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
@@ -9,5 +10,11 @@ io.on("connection", socket => {
     console.log(msg);
     io.emit("chat message", msg);
   });
+});*/
+var http = require('http');
+const port = 8001;
+var app = http.createServer(function(req,res){
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ a: 1 }));
 });
-server.listen(port, '192.168.43.168', () => console.log("server running on port:" + port));
+app.listen(port, '192.168.43.168', () => console.log("server running on port:" + port));
