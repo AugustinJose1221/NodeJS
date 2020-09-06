fca6c8fffc306576f8664fbf91d8867eb40cb8d0
Augustin Jose <augustinjose1221@gmail.com>
2020-09-06 11:48:15 +0530

    Minor Updates

--- main.js
+++ main.js
@@ -51,24 +51,52 @@ con.connect(function(err) {
 //Make requests using curl -H "Content-Type: application/json" --data @request.json http://192.168.43.168:8001
 app.use(express.json());
 app.post('/', function(request, response){
-    //console.log(request.body)
+
+    console.log("POST request recieved")
     var query = request.body.query;
     if (query=="Auth") {
+      var res = ""
+      //console.log("1");
       var deviceId = request.body.deviceId;      // your JSON
       con.query("select * from userdetails", function (err, results, fields) {
         if (err) throw err;
+        //console.log("2");
         for (result of results) {
           if (result.deviceId == deviceId){
-            console.log("Match Found")
-            response.send(JSON.stringify({ a:'True' }));
+            //console.log("Match Found")
+            res = JSON.stringify({ a:'True' });
+            break;
           }
           else{
-            console.log("Match Not Found")
-            response.send(JSON.stringify({ a:'False' }));
+            //console.log("Match Not Found")
+            res = JSON.stringify({ a:'False' });
           }
         }
+        console.log("Response Send")
+
+        response.send(res);
       });
     }
+
+    else if (query=="Login") {
+      var deviceId = request.body.deviceId;
+      var username = request.body.username;
+      con.query("select * from userdetails where username=?;", [username], function(err, results, fields){
+        if (err) throw err;
+        //console.log(results)
+        if (results.length==0) {
+          console.log("Adding new user")
+          con.query("insert into userdetails(deviceid, username) values(?,?)",[deviceId, username])
+          response.send(JSON.stringify({ a:'True' }));
+        }
+        else {
+          console.log("Username Taken")
+          response.send(JSON.stringify({ a:'False' }));
+        }
+      });
+
+    }
+
     //response.send(JSON.stringify({ a:'True' }));    // echo the result back
 });
 app.listen(8001,'192.168.43.168', () => console.log("server running on port:" + port));
